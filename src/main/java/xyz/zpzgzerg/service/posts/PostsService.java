40ce81534b7064b6d0bd8f517e4d32fb71bdea60
zpzgzerg <zpzgzerg@naver.com>
2020-04-22 22:10:14 +0900

    머스테치 관련 커밋

--- src/main/java/xyz/zpzgzerg/service/posts/PostsService.java
+++ src/main/java/xyz/zpzgzerg/service/posts/PostsService.java
@@ -5,9 +5,14 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import xyz.zpzgzerg.domain.posts.Posts;
 import xyz.zpzgzerg.domain.posts.PostsRepository;
+import xyz.zpzgzerg.web.dto.PostsListResponseDto;
 import xyz.zpzgzerg.web.dto.PostsResponseDto;
 import xyz.zpzgzerg.web.dto.PostsSaveRequestDto;
 import xyz.zpzgzerg.web.dto.PostsUpdateRequestDto;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,5 +35,18 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 }
---
+++ src/test/java/xyz/zpzgzerg/web/indexControllerTest.java
@@ -0,0 +1,27 @@
+package xyz.zpzgzerg.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class indexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 메인페이지_로딩() {
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        assertThat(body).contains("스프링부트로 시작하는 웹서비스");
+    }
+
+}
