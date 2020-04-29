f87b162a7285f92fba297a68127a2c6dac4bfcba
zpzgzerg <zpzgzerg@naver.com>
2020-04-30 00:02:21 +0900

    oauth 구글 연동

--- src/main/java/xyz/zpzgzerg/web/IndexController.java
+++ src/main/java/xyz/zpzgzerg/web/IndexController.java
@@ -5,18 +5,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import xyz.zpzgzerg.config.auth.dto.SessionUser;
 import xyz.zpzgzerg.service.posts.PostsService;
 import xyz.zpzgzerg.web.dto.PostsResponseDto;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
